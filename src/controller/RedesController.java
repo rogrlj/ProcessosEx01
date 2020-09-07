a4c814890540a2c04a6106d1d7849eaa55cc8994
ROCK N ROLL <ROCK N ROLL@DESKTOP-F22AMDT>
2020-09-07 20:58:57 -0300

    Método de PING adicionado

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -17,6 +17,7 @@ public class RedesController {
 		return os;
 	}
 	
+//	Função para chamar processos no Windows
 	public String callProcessW (String process) {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("cmd /c");
@@ -25,13 +26,6 @@ public class RedesController {
 		return (buffer.toString());
 	}
 	
-	public String callProcessL (String process) {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("xfce4-terminal");
-		buffer.append(" ");
-		buffer.append(process);
-		return (buffer.toString());
-	}
 	
 //	Função que faz a chamada de configuração do IP
 	public void ip(String os) {
@@ -68,7 +62,7 @@ public class RedesController {
 		}
 		else {
 			try {
-				Process p = Runtime.getRuntime().exec(callProcessL("ifconfig"));
+				Process p = Runtime.getRuntime().exec("ifconfig");
 				InputStream fluxo = p.getInputStream();
 				InputStreamReader leitor = new InputStreamReader(fluxo);
 				BufferedReader buffer = new BufferedReader(leitor);
@@ -80,6 +74,62 @@ public class RedesController {
 					}
 					else if(linha.contains("inet")) {
 						System.out.println(linha);
+						linha = buffer.readLine();
+					}
+					else {
+						linha = buffer.readLine();
+					}
+						
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+//	Função que calcula o PING médio em 10 iterações
+	public void ping (String os) {
+		
+		System.out.println("PING médio: ");
+		if (os.contains("Windows")) {
+			try {
+				Process p = Runtime.getRuntime().exec(callProcessW("ping -n 10 www.google.com.br"));
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("ximo")){
+						System.out.println(linha.substring(41));
+						linha = buffer.readLine();
+					}
+					else {
+						linha = buffer.readLine();
+					}
+						
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			try {
+				Process p = Runtime.getRuntime().exec("ping -c 10 www.google.com.br");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("avg")){
+						System.out.println(linha.substring(30, 36) + "ms");
 						linha = buffer.readLine();
 					}
 					else {
--- src/view/Main.java
+++ src/view/Main.java
@@ -14,7 +14,8 @@ public class Main {
 		
 		System.out.println("\n====================================");
 		
-		redesController.ip(os);
+//		redesController.ip(os);
+		redesController.ping(os);
 
 	}
 
